b1cb85cc18d860f0de18717c8c30b768fe77966c
scov8 <luigiscovotto@libero.it>
2021-11-22 22:04:25 +0100

    fix package path

--- src/main/java/controllers/Controller.java
+++ src/main/java/controllers/Controller.java
@@ -3,6 +3,7 @@
  * @author Marco Plaitano
  * @date 22 Nov 2021
  */
+package src.main.java.controllers;
 
 public class Controller {
 
--- src/main/java/resources/Example.java
+++ src/main/java/resources/Example.java
@@ -3,6 +3,7 @@
  * @author Marco Plaitano
  * @date 22 Nov 2021
  */
+package src.main.java.resources;
 
 public class Example {
 
