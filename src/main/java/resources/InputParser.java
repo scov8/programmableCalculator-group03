0795b34fa149a4b443daae0816d1b83263f85903
marcoplaitano <marcoplaitano23@gmail.com>
2021-11-24 14:58:52 +0100

    added method to recognize numbers from string.

--- src/main/java/resources/InputParser.java
+++ src/main/java/resources/InputParser.java
@@ -12,6 +12,57 @@ package src.main.java.resources;
 public class InputParser {
 
     /**
+     * @brief Parse the given text to return the corresponding complex number.
+     *
+     *        The string has to be in the form 'a+bi' to be correctly parsed.
+     * @param text The string to parse.
+     * @return A `Number` instance or `null` if the string is incorrect.
+     */
+    public static Number parseNumber(String text) {
+        // Remove all spaces from the string.
+        text = text.strip();
+        text = text.replace(" ", "");
+        text = text.replace("j", "i");
+
+        // Check that the syntax is correct and follows the form: 'a+bj'.
+        // In particular:
+        // - a and/or b can be negative.
+        // - i can be replaced with j.
+        if (!text.matches("^[+-]?[0-9]\\d*(\\.\\d+)?$"))
+            if (!text.matches("^[+-]?[0-9]\\d*(\\.\\d+)?([+-][0-9]\\d*(\\.\\d+)?i)?$"))
+                if (!text.matches("^[+-]?[0-9]\\d*(\\.\\d+)?i$"))
+                    return null;
+
+        String a, b;
+
+        // Look for the imaginary part sign's position in the string.
+        int signPos = text.indexOf("+", 1);
+        if (signPos < 0)
+            signPos = text.indexOf("-", 1);
+        // If no sign to divide real and imaginary part is found it means only
+        // one of them is present in the string.
+        if (signPos < 0) {
+            // If 'i' is in the string the number is imaginary, otherwise it is
+            // real.
+            if (text.indexOf("i") > 0) {
+                a = "0";
+                b = text.substring(0, text.length() - 1);
+            } else {
+                a = text;
+                b = "0";
+            }
+        } else {
+            // Get substring representing real part of the number, sign included.
+            a = text.substring(0, signPos);
+            // Get substring representing imaginary part of the number, sign
+            // included, i/j excluded.
+            b = text.substring(signPos, text.length() - 1);
+        }
+
+        return new Number(Double.parseDouble(a), Double.parseDouble(b));
+    }
+
+    /**
      * @brief Check whether the given string is a valid operation.
      * @param text The string to validate.
      * @return `true` if the string matches one of the operations; `false`
--- test/InputParserTest.java
+++ test/InputParserTest.java
@@ -10,8 +10,51 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 import src.main.java.resources.InputParser;
+import src.main.java.resources.Number;
 
 public class InputParserTest {
+    @Test
+    public void testParseNumber() {
+        Number n = new Number(2, 5);
+        assertEquals(n, InputParser.parseNumber("2 + 5i"));
+        n.setValues(-10, 5);
+        assertEquals(n, InputParser.parseNumber("-10 + 5j"));
+        n.setValues(-10, -25);
+        assertEquals(n, InputParser.parseNumber("-10 -25i"));
+        n.setValues(123, -7);
+        assertEquals(n, InputParser.parseNumber("+123 -7i"));
+        n.setValues(12.3, -7.3);
+        assertEquals(n, InputParser.parseNumber("12.3 - 7.3j"));
+        n.setValues(-12.3, -7.3);
+        assertEquals(n, InputParser.parseNumber("-12.3 - 7.3i"));
+
+        n.setValues(0, 0);
+        assertEquals(n, InputParser.parseNumber("0 + 0 i"));
+        n.setValues(0, 0);
+        assertEquals(n, InputParser.parseNumber("0"));
+        n.setValues(0, 0);
+        assertEquals(n, InputParser.parseNumber("0j"));
+        n.setValues(0, 0);
+        assertEquals(n, InputParser.parseNumber("0i"));
+        n.setValues(3, 0);
+        assertEquals(n, InputParser.parseNumber("3"));
+        n.setValues(-3, 0);
+        assertEquals(n, InputParser.parseNumber("-3"));
+        n.setValues(0, 12);
+        assertEquals(n, InputParser.parseNumber("12i"));
+        n.setValues(0, -12.5);
+        assertEquals(n, InputParser.parseNumber("-12.5i"));
+
+        assertEquals(null, InputParser.parseNumber("12a"));
+        assertEquals(null, InputParser.parseNumber("hello world"));
+        assertEquals(null, InputParser.parseNumber("3 + 2"));
+        assertEquals(null, InputParser.parseNumber("3 +- 2"));
+        assertEquals(null, InputParser.parseNumber("3 +- 2i"));
+        assertEquals(null, InputParser.parseNumber("-3 -+ 212i"));
+        assertEquals(null, InputParser.parseNumber("3a + 212i"));
+        assertEquals(null, InputParser.parseNumber("a2 + 212i"));
+    }
+
     @Test
     public void testIsOperation() {
         assertTrue(InputParser.isOperation("+"));
