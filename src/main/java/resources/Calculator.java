74905a64840dd8323f6932ff901fa3b8d603790d
marcoplaitano <marcoplaitano23@gmail.com>
2021-11-30 11:36:20 +0100

    Removed Operations.java
    Added Unrecognized Operation Exception.
    Updated test on Calculator class to take into account new exception.

--- src/main/java/resources/Calculator.java
+++ src/main/java/resources/Calculator.java
@@ -1,9 +1,19 @@
 package src.main.java.resources;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
+import src.main.java.exceptions.IndeterminateFormException;
 import src.main.java.exceptions.NotEnoughOperandsException;
-import src.main.java.operations.Operations;
+import src.main.java.exceptions.UnrecognizedOperationException;
+import src.main.java.operations.DifferenceOperation;
+import src.main.java.operations.DivisionOperation;
+import src.main.java.operations.MultiplicationOperation;
+import src.main.java.operations.Operation;
+import src.main.java.operations.SignInversionOperation;
+import src.main.java.operations.SquareRootOperation;
+import src.main.java.operations.SumOperation;
 
 /**
  * @file Calculator.java
@@ -15,9 +25,16 @@ import src.main.java.operations.Operations;
  * @brief This class handles the operation and the operands in the stack.
  */
 public class Calculator {
+    private Map<String, Operation> operations;
 
     public Calculator() {
-
+        operations = new HashMap<>();
+        operations.put("+", new SumOperation());
+        operations.put("-", new DifferenceOperation());
+        operations.put("*", new MultiplicationOperation());
+        operations.put("/", new DivisionOperation());
+        operations.put("+-", new SignInversionOperation());
+        operations.put("sqrt", new SquareRootOperation());
     }
 
     /**
@@ -25,56 +42,10 @@ public class Calculator {
      * @param stack The stack of complex numbers.
      * @param op    String representing the operation to execute.
      */
-    public void runOperation(Stack<ComplexNumber> stack, String op) throws Exception {
-        ComplexNumber result = null;
-        ComplexNumber a, b;
-
-        switch (op) {
-            case "+":
-                if (stack.size() < 2)
-                    throw new NotEnoughOperandsException();
-                b = stack.pop();
-                a = stack.pop();
-                result = Operations.sum(a, b);
-                break;
-            case "-":
-                if (stack.size() < 2)
-                    throw new NotEnoughOperandsException();
-                b = stack.pop();
-                a = stack.pop();
-                result = Operations.difference(a, b);
-                break;
-            case "*":
-                if (stack.size() < 2)
-                    throw new NotEnoughOperandsException();
-                b = stack.pop();
-                a = stack.pop();
-                result = Operations.multiplication(a, b);
-                break;
-            case "/":
-                if (stack.size() < 2)
-                    throw new NotEnoughOperandsException();
-                b = stack.pop();
-                a = stack.pop();
-                result = Operations.division(a, b);
-                break;
-            case "+-":
-                if (stack.size() < 1)
-                    throw new NotEnoughOperandsException();
-                a = stack.pop();
-                result = Operations.signInvertion(a);
-                break;
-            case "sqrt":
-                if (stack.size() < 1)
-                    throw new NotEnoughOperandsException();
-                a = stack.pop();
-                result = Operations.squareRoot(a);
-                break;
-            default:
-                break;
-        }
-
-        if (result != null)
-            stack.push(result);
+    public void runOperation(Stack<ComplexNumber> stack, String opString) throws Exception {
+        Operation op = operations.get(opString);
+        if (op == null)
+            throw new UnrecognizedOperationException();
+        op.execute(stack);
     }
 }
