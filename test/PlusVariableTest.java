43fb15d7997175216578048412af714fb141b565
scov8 <luigiscovotto@libero.it>
2021-12-02 17:27:15 +0100

    plus variable and his test

---
+++ test/PlusVariableTest.java
@@ -0,0 +1,79 @@
+package test;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import src.main.java.resources.ComplexNumber;
+import src.main.java.resources.Variables;
+import src.main.java.variables.PlusVariable;
+
+import java.util.Stack;
+
+import static org.junit.Assert.*;
+
+/**
+ * @file PlusVariableTest.java
+ * @author Luigi Scovotto
+ * @date 01 Dic 2021
+ */
+
+public class PlusVariableTest {
+    private static PlusVariable plusVariable;
+    private static ComplexNumber number0;
+    private static ComplexNumber number1;
+    private static ComplexNumber number2;
+    private static ComplexNumber number3;
+    private static ComplexNumber number4;
+    private static ComplexNumber number5;
+    private static ComplexNumber number6;
+
+    private static ComplexNumber result30;
+    private static ComplexNumber result41;
+    private static ComplexNumber result52;
+    private static ComplexNumber result63;
+
+    private static Stack<ComplexNumber> stack;
+    private static Variables variable;
+
+    @BeforeClass
+    public static void setUp() {
+        plusVariable = new PlusVariable();
+        number0 = new ComplexNumber(0, 0);
+        number1 = new ComplexNumber(3, 4);
+        number2 = new ComplexNumber(-2, -1);
+        number3 = new ComplexNumber(7, -8);
+        number4 = new ComplexNumber(-10, 11);
+        number5 = new ComplexNumber(8, 0);
+        number6 = new ComplexNumber(0, -4);
+        variable = new Variables();
+        stack = new Stack<>();
+
+        result30 = new ComplexNumber(7, -8);
+        result41 = new ComplexNumber(-7, 15);
+        result52 = new ComplexNumber(6, -1);
+        result63 = new ComplexNumber(7, -12);
+    }
+
+    @Test
+    public void testMinusVariableTest() {
+        stack.push(number3);
+        stack.push(number2);
+        stack.push(number1);
+        stack.push(number0);
+        variable.set('a', number3);
+        variable.set('b', number4);
+        variable.set('c', number5);
+        variable.set('d', number6);
+        plusVariable.execute(variable, stack, 'a');
+        stack.pop();
+        plusVariable.execute(variable, stack, 'b');
+        stack.pop();
+        plusVariable.execute(variable, stack, 'c');
+        stack.pop();
+        plusVariable.execute(variable, stack, 'd');
+        assertEquals(result30, variable.get('a'));
+        assertEquals(result41, variable.get('b'));
+        assertEquals(result52, variable.get('c'));
+        assertEquals(result63, variable.get('d'));
+    }
+}
