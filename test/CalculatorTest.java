154561d4105668335309b83392c13c110aaf1daf
Gerardo Rosa <92717241+gerardorosa@users.noreply.github.com>
2021-12-10 11:34:58 +0100

    Update CalculatorTest.java

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -10,133 +10,131 @@ import org.junit.*;
 
 import static org.junit.Assert.*;
 
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.List;
 
+import src.main.java.exceptions.IndeterminateFormException;
 import src.main.java.exceptions.NotEnoughOperandsException;
-import src.main.java.operations.DifferenceOperation;
-import src.main.java.operations.DivisionOperation;
-import src.main.java.operations.MultiplicationOperation;
-import src.main.java.operations.SignInversionOperation;
-import src.main.java.operations.SquareRootOperation;
-import src.main.java.operations.SumOperation;
+import src.main.java.exceptions.UnrecognizedInputException;
+import src.main.java.exceptions.VariableWithoutValueException;
+import src.main.java.exceptions.UserOperationExecutionException;
+import src.main.java.operations.OperationsMap;
 import src.main.java.resources.Calculator;
 import src.main.java.resources.ComplexNumber;
+import src.main.java.userOperations.UserOperation;
 
 public class CalculatorTest {
     private static Calculator c;
-    private static Stack<ComplexNumber> stack;
+    private static List<ComplexNumber> list;
+    private static ComplexNumber a;
+    private static ComplexNumber b;
 
     @BeforeClass
     public static void setUp() {
         c = new Calculator();
-        stack = new Stack<>();
+        list = new LinkedList<>();
+        a = new ComplexNumber(1,3.5);
+        b = new ComplexNumber(2.4,-8.024);
     }
 
     @Before
     public void initStack() {
-        stack.push(new ComplexNumber(1, 3.5));
-        stack.push(new ComplexNumber(2.4, -8.024));
+        c.run("1+3.5i");
+        c.run("2.4-8.024i");
+    }
+    @Test
+    public void testGetTopKNumbers(){
+        list = c.getTopKNumbers(2);
+        assertEquals(a, list.get(0));
+        assertEquals(b, list.get(1));
     }
 
     @Test
-    public void testRunOperationOnSignInversion() throws Exception {
-        int size = stack.size();
-        SignInversionOperation operation = new SignInversionOperation();
-        Stack<ComplexNumber> stack2 = new Stack<>();
-        stack2.addAll(stack);
+    public void testRunOperationOnStackOperation() throws Exception {
+        ComplexNumber number = new ComplexNumber(-2.4,+8.024);
         // sign invertion. Top inverts its sign, size does not change.
-        c.runStackOperation(stack, "+-");
-        operation.execute(stack2);
-        assertEquals(stack2.peek(), stack.peek());
-        assertEquals(size, stack.size());
+        c.run("+-");
+        list = c.getTopKNumbers(1);
+        assertEquals(number, list.get(0));
     }
 
     @Test
-    public void testRunOperationOnSquareRoot() throws Exception {
-        int size = stack.size();
-        SquareRootOperation operation = new SquareRootOperation();
-        Stack<ComplexNumber> stack2 = new Stack<>();
-        stack2.addAll(stack);
-        // square root. Top gets square rooted, size does not change.
-        c.runStackOperation(stack, "sqrt");
-        operation.execute(stack2);
-        assertEquals(stack2.peek(), stack.peek());
-        assertEquals(size, stack.size());
+    public void testRunOperationOnVariables() throws Exception {
+        c.run(">a");
+        assertEquals(b, c.getVariable('a'));
+        c.run("drop");
+        c.run(">b");
+        assertEquals(a, c.getVariable('b'));
+        c.run("clear");
+        c.run("<b");
+        list = c.getTopKNumbers(1);
+        assertEquals(a, list.get(0));
     }
 
     @Test
-    public void testRunOperationOnSum() throws Exception {
-        int size = stack.size();
-        SumOperation operation = new SumOperation();
-        Stack<ComplexNumber> stack2 = new Stack<>();
-        stack2.addAll(stack);
-        // Sum. Top 2 elements are removed and result pushed to the stack.
-        c.runStackOperation(stack, "+");
-        operation.execute(stack2);
-        assertEquals(stack2.peek(), stack.peek());
-        assertEquals(size - 1, stack.size());
+    public void testRunOperationOnUserDefined() throws Exception {
+        c.run("1+j");
+        UserOperation op = new UserOperation("hello", "+ -");
+        OperationsMap map = OperationsMap.getInstance();
+        map.addUserDefinedOperation(op);
+        c.run("hello");
+        list = c.getTopKNumbers(1);
+        ComplexNumber x = new ComplexNumber(-2.4,10.524);
+        assertEquals(x, list.get(0));
     }
 
     @Test
-    public void testRunOperationOnDifference() throws Exception {
-        int size = stack.size();
-        DifferenceOperation operation = new DifferenceOperation();
-        Stack<ComplexNumber> stack2 = new Stack<>();
-        stack2.addAll(stack);
-        // Difference. Top 2 elements are removed and result pushed to the stack.
-        c.runStackOperation(stack, "-");
-        operation.execute(stack2);
-        assertEquals(stack2.peek(), stack.peek());
-        assertEquals(size - 1, stack.size());
-    }
+    public void testUnrecognizedInputException() throws Exception {
 
-    @Test
-    public void testRunOperationOnMultiplication() throws Exception {
-        int size = stack.size();
-        MultiplicationOperation operation = new MultiplicationOperation();
-        Stack<ComplexNumber> stack2 = new Stack<>();
-        stack2.addAll(stack);
-        // Multiplication. Top 2 elements are removed and result pushed to the stack.
-        c.runStackOperation(stack, "*");
-        operation.execute(stack2);
-        assertEquals(stack2.peek(), stack.peek());
-        assertEquals(size - 1, stack.size());
+        assertThrows(UnrecognizedInputException.class, () -> c.run("blablabla"));
+        assertThrows(UnrecognizedInputException.class, () -> c.run("topolino"));
+        assertThrows(UnrecognizedInputException.class, () -> c.run("pippo"));
+        assertThrows(UnrecognizedInputException.class, () -> c.run("pluto"));
+        assertThrows(UnrecognizedInputException.class, () -> c.run("franco126"));
     }
-
     @Test
-    public void testRunOperationOnDivision() throws Exception {
-        int size = stack.size();
-        DivisionOperation operation = new DivisionOperation();
-        Stack<ComplexNumber> stack2 = new Stack<>();
-        stack2.addAll(stack);
-        // Division. Top 2 elements are removed and result pushed to the stack.
-        c.runStackOperation(stack, "/");
-        operation.execute(stack2);
-        assertEquals(stack2.peek(), stack.peek());
-        assertEquals(size - 1, stack.size());
-    }
-
-    @Test
-    public void testNotEnoughOperandsExceptionOnRunOperation() throws Exception {
+    public void testNotEnoughOperandsException() throws Exception {
+        c.run("+");
         // only 1 item in the stack.
-        stack.clear();
-        stack.push(new ComplexNumber(-2.8, 11));
-        assertEquals(1, stack.size());
-
-        assertThrows(NotEnoughOperandsException.class, () -> c.runStackOperation(stack, "+"));
-        assertThrows(NotEnoughOperandsException.class, () -> c.runStackOperation(stack, "-"));
-        assertThrows(NotEnoughOperandsException.class, () -> c.runStackOperation(stack, "*"));
-        assertThrows(NotEnoughOperandsException.class, () -> c.runStackOperation(stack, "/"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run("+"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run("-"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run("*"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run("/"));
 
         // no items in the stack.
-        stack.clear();
-        assertEquals(0, stack.size());
+        c.run("clear");
 
-        assertThrows(NotEnoughOperandsException.class, () -> c.runStackOperation(stack, "+"));
-        assertThrows(NotEnoughOperandsException.class, () -> c.runStackOperation(stack, "-"));
-        assertThrows(NotEnoughOperandsException.class, () -> c.runStackOperation(stack, "*"));
-        assertThrows(NotEnoughOperandsException.class, () -> c.runStackOperation(stack, "/"));
-        assertThrows(NotEnoughOperandsException.class, () -> c.runStackOperation(stack, "+-"));
-        assertThrows(NotEnoughOperandsException.class, () -> c.runStackOperation(stack, "sqrt"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run("+"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run("-"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run("*"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run("/"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run("+-"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run("sqrt"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run("drop"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run("dup"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run("swap"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run("over"));
+        assertThrows(NotEnoughOperandsException.class, () -> c.run(">a"));
+    }
+    @Test
+    public void testIndeterminateFormException() throws Exception {
+        c.run("0");
+        c.run("0");
+        assertThrows(IndeterminateFormException.class, () -> c.run("/"));
+    }
+
+    @Test
+    public void testVariableWithoutValueException() throws Exception {
+        assertThrows(VariableWithoutValueException.class, () -> c.run("<a"));
+        assertThrows(VariableWithoutValueException.class, () -> c.run("+a"));
+        assertThrows(VariableWithoutValueException.class, () -> c.run("-a"));
+    }
+
+    @Test
+    public void testUserOperationExecutionException() throws Exception {
+        UserOperation op = new UserOperation("hello", "+ - * /");
+        OperationsMap map = OperationsMap.getInstance();
+        map.addUserDefinedOperation(op);
+        assertThrows(UserOperationExecutionException.class, () -> c.run("hello"));
     }
 }
